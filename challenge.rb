8f18f1612cc53d3b52516a9bf609153ee7df8f6e
francescadal <francescadalembert@icloud.com>
2015-10-20 09:48:51 -0400

    done

--- challenge.rb
+++ challenge.rb
@@ -18,16 +18,22 @@ else
 end
 
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+true
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+false
 # 2.4 Substitue 63.4 < 64. What's the output?
+true
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
 # 3.1. determine if a string is included another another string?
+include?
 # 3.2. determine if the length of a string?
+.length
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
+gsub
 # change the string "eels in my hovercraft"
 
 
@@ -36,11 +42,20 @@ end
 # to the string should be made in-place. (Unless told otherwise, like in 4.3.)
 
 vehicle = "4 wheel Bus"
+puts vehicle
+
+vehicle.gsub(/4/, "6")
+puts vehicle
+
+vehicle.gsub(/bus/, "tank") << "that shoots rainbow bunnies"
+
 
 # 4.1 to "6 wheel Bus"
 # 4.2 to "6 wheel tank"
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
+
+
 
 # Question 5 : Loop
 # - write a loop that guesses your age.
@@ -50,9 +65,27 @@ vehicle = "4 wheel Bus"
 # - aaaah maaan c'mon, huh, WTF
 # - I'm not kidding, say unkind works to a user that thinks your 10 years older
 #   than you are.
+age = 24
+guess = 0
+tries = 0
+max_tries = 4
+
+while guess != number
+  puts "guess how old i am"
+  guess = gets.chomp.to_i
+  unless tries < max_tries
+    puts "No more tries. Sorry"
+    break
+  end
+  tries += 1
+end
+
+puts "That is exactly how old I am." if guess == age
+puts "Do i look like a grandma?" if guess < age
 
 # Bonus Question: Who said this?
 
 # The great enemy of the truth is very often not the lie -- deliberate, contrived
 # and dishonest, but the myth, persistent, persuasive, and unrealistic.
 # Belief in myths allows the comfort of opinion without the discomfort of thought.
+JFK
